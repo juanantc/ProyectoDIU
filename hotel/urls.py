ce81d74e6feb24ec4809354cae21e7cb7277e7f3
juanantc <juanantc@gmail.com>
2017-05-19 18:47:32 +0200

    Añadidas secciones y URL dispatcher

--- hotel/urls.py
+++ hotel/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^servicios$', views.servicios, name='servicios'),
+    url(r'^galeria$', views.galeria, name='galeria'),
+    url(r'^informacion$', views.informacion, name='informacion'),
+    url(r'^foro$', views.foro, name='foro'),
+
+
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'hotel/index.html')
+
+def servicios(request):
+    return render(request, 'hotel/servicios.html')
+
+def galeria(request):
+    return render(request, 'hotel/galeria.html')
+
+def informacion(request):
+    return render(request, 'hotel/informacion.html')
+
+def foro(request):
+    return render(request, 'hotel/foro.html')
